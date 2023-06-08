29846290fd0e3eb0f338330ee93f2d01e2386194
Reyhanerezaee <135907664+Reyhanerezaee@users.noreply.github.com>
2023-06-08 09:02:14 +0330

    Update bmi.py

--- bmi.py
+++ bmi.py
@@ -5,4 +5,11 @@ weight = int(input("Enter your weight in kilograms: "))
 
 bmi = weight / (height * height)
 
-print(bmi)
+if bmi < 18.5:
+    print("Underweight")
+elif bmi > 18.5 and bmi < 24.9:
+    print("Healthy")
+elif bmi > 25 and bmi < 29.9:
+    print("Overweight")
+elif bmi > 30:
+    print("Fat")
